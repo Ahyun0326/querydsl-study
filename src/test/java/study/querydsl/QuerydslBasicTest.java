e9b3c6489633d17901ea2be2a7b76586c1f8a4db
YESEO KIM <chinkl0110@naver.com>
2024-06-21 15:15:39 +0900

    feat: 서브 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -17,6 +18,7 @@ import study.querydsl.entity.Team;
 
 import java.util.List;
 
+import static com.querydsl.jpa.JPAExpressions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.*;
 import static study.querydsl.entity.QTeam.team;
@@ -290,27 +292,109 @@ public class QuerydslBasicTest {
         em.flush();
         em.clear();
 
-      Member findMember = queryFactory
+        Member findMember = queryFactory
           .selectFrom(member)
           .where(member.username.eq("member1"))
           .fetchOne();
 
-      boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());// 이미 로링된 엔티티인지, 로딩되지 않은 엔티티인지 알려줌
-      assertThat(loaded).as("페치 조인 미적용").isFalse();
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());// 이미 로링된 엔티티인지, 로딩되지 않은 엔티티인지 알려줌
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
     }
 
-  @Test
-  public void fetchJoinUse() {
-    em.flush();
-    em.clear();
+    @Test
+    public void fetchJoinUse() {
+        em.flush();
+        em.clear();
 
-    Member findMember = queryFactory
-        .selectFrom(member)
-        .join(member.team, team).fetchJoin()
-        .where(member.username.eq("member1"))
-        .fetchOne();
+        Member findMember = queryFactory
+            .selectFrom(member)
+            .join(member.team, team).fetchJoin()
+            .where(member.username.eq("member1"))
+            .fetchOne();
 
-    boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());// 이미 로링된 엔티티인지, 로딩되지 않은 엔티티인지 알려줌
-    assertThat(loaded).as("페치 조인 미적용").isTrue();
-  }
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());// 이미 로링된 엔티티인지, 로딩되지 않은 엔티티인지 알려줌
+        assertThat(loaded).as("페치 조인 미적용").isTrue();
+    }
+
+    /**
+     * 나이가 가장 많은 회원 조회
+     */
+    @Test
+    public void subQuery() {
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .where(member.age.eq(
+                select(memberSub.age.max())
+                    .from(memberSub)
+            ))
+            .fetch();
+
+        assertThat(result).extracting("age")
+            .containsExactly(40);
+    }
+
+    /**
+     * 나이가 평균 나이 이상인 회원
+     */
+
+    @Test
+    public void subQueryGoe() {
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .where(member.age.goe(
+                select(memberSub.age.avg())
+                    .from(memberSub)
+            ))
+            .fetch();
+
+        assertThat(result).extracting("age")
+            .containsExactly(30, 40);
+    }
+
+    /**
+     * 서브쿼리 여러 건 처리, in 사용
+     */
+    @Test
+    public void subQueryIn() {
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .where(member.age.in(
+                select(memberSub.age)
+                    .from(memberSub)
+                    .where(memberSub.age.gt(10))    // 10살 초과인 경우
+            ))
+            .fetch();
+
+        assertThat(result).extracting("age")
+            .containsExactly(20, 30, 40);
+
+    }
+
+    /**
+     * select 절에 subQuery 사용
+     */
+    @Test
+    public void selectSubQuery() {
+        QMember memberSub = new QMember("memberSub");
+
+        List<Tuple> result = queryFactory
+            .select(member.username,
+                select(memberSub.age.avg())
+                    .from(memberSub))
+            .from(member)
+            .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
 }
