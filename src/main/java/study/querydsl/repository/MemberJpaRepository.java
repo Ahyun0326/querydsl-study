0a397b440c44c2182aca1571cf463eba4688842e
YESEO KIM <chinkl0110@naver.com>
2024-06-23 15:59:40 +0900

    feat: Querydsl 추가

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package study.querydsl.repository;
 
+import static study.querydsl.entity.QMember.*;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -32,10 +34,22 @@ public class MemberJpaRepository {
         .getResultList();
   }
 
+  public List<Member> findAll_Querydsl() {
+    return queryFactory
+        .selectFrom(member)
+        .fetch();
+  }
+
   public List<Member> findByUsername(String username) {
     return em.createQuery("select m from Member m where m.username = :username", Member.class)
         .setParameter("username", username)
         .getResultList();
   }
 
+  public List<Member> findByUsername_Querydsl(String username) {
+    return queryFactory
+        .selectFrom(member)
+        .where(member.username.eq(username))
+        .fetch();
+  }
 }
