8f2b263f067722170ebe2c0c9d6fc1f32485abee
YESEO KIM <chinkl0110@naver.com>
2024-06-23 19:20:46 +0900

    feat: 스프링 데이터 페이징 활용 - Querydsl 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,11 +1,14 @@
 package study.querydsl.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCond;
 import study.querydsl.dto.MemberTeamDto;
 
 public interface MemberRepositoryCustom {
 
   List<MemberTeamDto> search(MemberSearchCond condition);
+  Page<MemberTeamDto> searchWithPaging(MemberSearchCond condition, Pageable pageable);
 
 }
--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -4,10 +4,14 @@ import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCond;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -39,6 +43,46 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
         )
         .fetch();
   }
+
+  @Override
+  public Page<MemberTeamDto> searchWithPaging(MemberSearchCond condition, Pageable pageable) {
+
+    // 데이터 조회 쿼리 (페이징 적용)
+    List<MemberTeamDto> content = queryFactory
+        .select(new QMemberTeamDto(
+            member.id,
+            member.username,
+            member.age,
+            team.id,
+            team.name))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(
+            usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoeEq(condition.getAgeGoe()),
+            ageLoeEq(condition.getAgeLoe())
+        )
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())  //한 번 조회 시 몇 개까지 조회할 건지 지정
+        .fetch();
+
+    // count 쿼리 (조건에 부합하는 로우의 총 개수를 얻는 것이기 때문에 페이징 미적용)
+    Long total = queryFactory
+        .select(member.count()) // SQL 상으로는 count(member.id)와 동일
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(
+            usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoeEq(condition.getAgeGoe()),
+            ageLoeEq(condition.getAgeLoe())
+        )
+        .fetchOne();
+
+    return new PageImpl<>(content, pageable, total);
+  }
+
   private BooleanExpression usernameEq(String username) {
     return hasText(username) ? member.username.eq(username): null;
   }
